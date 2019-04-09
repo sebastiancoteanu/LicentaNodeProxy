c5955eaf81bece5bc87e02336c14797575b50dea
Sebastian Coteanu <sebastian.coteanu@mindit.ro>
2019-04-09 20:30:00 +0300

    openshift config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var request = require('request');
-var port = process.env.PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var app = express();
 
-app.listen(port, function () {
+app.listen(server_port, server_ip_address, function () {
     console.log(`Example app listening on port !`);
 });
 
