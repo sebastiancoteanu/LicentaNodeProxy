c6355e51d39d07930bed067c63def4deb5e5f12a
Sebastian Coteanu <sebastian.coteanu@mindit.ro>
2019-04-12 00:56:09 +0300

    Change again the mock data

--- index.js
+++ index.js
@@ -17,123 +17,8 @@ app.listen(server_port, function () {
     console.log("Server is running");
 });
 
-let mockJson = {
-    "html_attributions" : [],
-    "results" : [
-       {
-          "geometry" : {
-             "location" : {
-                "lat" : -33.870775,
-                "lng" : 151.199025
-             }
-          },
-          "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
-          "id" : "21a0b251c9b8392186142c798263e289fe45b4aa",
-          "name" : "Rhythmboat Cruises",
-          "opening_hours" : {
-             "open_now" : true
-          },
-          "photos" : [
-             {
-                "height" : 270,
-                "html_attributions" : [],
-                "photo_reference" : "CnRnAAAAF-LjFR1ZV93eawe1cU_3QNMCNmaGkowY7CnOf-kcNmPhNnPEG9W979jOuJJ1sGr75rhD5hqKzjD8vbMbSsRnq_Ni3ZIGfY6hKWmsOf3qHKJInkm4h55lzvLAXJVc-Rr4kI9O1tmIblblUpg2oqoq8RIQRMQJhFsTr5s9haxQ07EQHxoUO0ICubVFGYfJiMUPor1GnIWb5i8",
-                "width" : 519
-             }
-          ],
-          "place_id" : "ChIJyWEHuEmuEmsRm9hTkapTCrk",
-          "scope" : "GOOGLE",
-          "alt_ids" : [
-             {
-                "place_id" : "D9iJyWEHuEmuEmsRm9hTkapTCrk",
-                "scope" : "APP"
-             }
-          ],
-          "reference" : "CoQBdQAAAFSiijw5-cAV68xdf2O18pKIZ0seJh03u9h9wk_lEdG-cP1dWvp_QGS4SNCBMk_fB06YRsfMrNkINtPez22p5lRIlj5ty_HmcNwcl6GZXbD2RdXsVfLYlQwnZQcnu7ihkjZp_2gk1-fWXql3GQ8-1BEGwgCxG-eaSnIJIBPuIpihEhAY1WYdxPvOWsPnb2-nGb6QGhTipN0lgaLpQTnkcMeAIEvCsSa0Ww",
-          "types" : [ "travel_agency", "restaurant", "food", "establishment" ],
-          "vicinity" : "Pyrmont Bay Wharf Darling Dr, Sydney"
-       },
-       {
-          "geometry" : {
-             "location" : {
-                "lat" : -33.866891,
-                "lng" : 151.200814
-             }
-          },
-          "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
-          "id" : "45a27fd8d56c56dc62afc9b49e1d850440d5c403",
-          "name" : "Private Charter Sydney Habour Cruise",
-          "photos" : [
-             {
-                "height" : 426,
-                "html_attributions" : [],
-                "photo_reference" : "CnRnAAAAL3n0Zu3U6fseyPl8URGKD49aGB2Wka7CKDZfamoGX2ZTLMBYgTUshjr-MXc0_O2BbvlUAZWtQTBHUVZ-5Sxb1-P-VX2Fx0sZF87q-9vUt19VDwQQmAX_mjQe7UWmU5lJGCOXSgxp2fu1b5VR_PF31RIQTKZLfqm8TA1eynnN4M1XShoU8adzJCcOWK0er14h8SqOIDZctvU",
-                "width" : 640
-             }
-          ],
-          "place_id" : "ChIJqwS6fjiuEmsRJAMiOY9MSms",
-          "scope" : "GOOGLE",
-          "reference" : "CpQBhgAAAFN27qR_t5oSDKPUzjQIeQa3lrRpFTm5alW3ZYbMFm8k10ETbISfK9S1nwcJVfrP-bjra7NSPuhaRulxoonSPQklDyB-xGvcJncq6qDXIUQ3hlI-bx4AxYckAOX74LkupHq7bcaREgrSBE-U6GbA1C3U7I-HnweO4IPtztSEcgW09y03v1hgHzL8xSDElmkQtRIQzLbyBfj3e0FhJzABXjM2QBoUE2EnL-DzWrzpgmMEulUBLGrtu2Y",
-          "types" : [ "restaurant", "food", "establishment" ],
-          "vicinity" : "Australia"
-       },
-       {
-          "geometry" : {
-             "location" : {
-                "lat" : -33.870943,
-                "lng" : 151.190311
-             }
-          },
-          "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
-          "id" : "30bee58f819b6c47bd24151802f25ecf11df8943",
-          "name" : "Bucks Party Cruise",
-          "opening_hours" : {
-             "open_now" : true
-          },
-          "photos" : [
-             {
-                "height" : 600,
-                "html_attributions" : [],
-                "photo_reference" : "CnRnAAAA48AX5MsHIMiuipON_Lgh97hPiYDFkxx_vnaZQMOcvcQwYN92o33t5RwjRpOue5R47AjfMltntoz71hto40zqo7vFyxhDuuqhAChKGRQ5mdO5jv5CKWlzi182PICiOb37PiBtiFt7lSLe1SedoyrD-xIQD8xqSOaejWejYHCN4Ye2XBoUT3q2IXJQpMkmffJiBNftv8QSwF4",
-                "width" : 800
-             }
-          ],
-          "place_id" : "ChIJLfySpTOuEmsRsc_JfJtljdc",
-          "scope" : "GOOGLE",
-          "reference" : "CoQBdQAAANQSThnTekt-UokiTiX3oUFT6YDfdQJIG0ljlQnkLfWefcKmjxax0xmUpWjmpWdOsScl9zSyBNImmrTO9AE9DnWTdQ2hY7n-OOU4UgCfX7U0TE1Vf7jyODRISbK-u86TBJij0b2i7oUWq2bGr0cQSj8CV97U5q8SJR3AFDYi3ogqEhCMXjNLR1k8fiXTkG2BxGJmGhTqwE8C4grdjvJ0w5UsAVoOH7v8HQ",
-          "types" : [ "restaurant", "food", "establishment" ],
-          "vicinity" : "37 Bank St, Pyrmont"
-       },
-       {
-          "geometry" : {
-             "location" : {
-                "lat" : -33.867591,
-                "lng" : 151.201196
-             }
-          },
-          "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
-          "id" : "a97f9fb468bcd26b68a23072a55af82d4b325e0d",
-          "name" : "Australian Cruise Group",
-          "opening_hours" : {
-             "open_now" : true
-          },
-          "photos" : [
-             {
-                "height" : 242,
-                "html_attributions" : [],
-                "photo_reference" : "CnRnAAAABjeoPQ7NUU3pDitV4Vs0BgP1FLhf_iCgStUZUr4ZuNqQnc5k43jbvjKC2hTGM8SrmdJYyOyxRO3D2yutoJwVC4Vp_dzckkjG35L6LfMm5sjrOr6uyOtr2PNCp1xQylx6vhdcpW8yZjBZCvVsjNajLBIQ-z4ttAMIc8EjEZV7LsoFgRoU6OrqxvKCnkJGb9F16W57iIV4LuM",
-                "width" : 200
-             }
-          ],
-          "place_id" : "ChIJrTLr-GyuEmsRBfy61i59si0",
-          "scope" : "GOOGLE",
-          "reference" : "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
-          "types" : [ "travel_agency", "restaurant", "food", "establishment" ],
-          "vicinity" : "32 The Promenade, King Street Wharf 5, Sydney"
-       }
-    ],
-    "status" : "OK"
- }
+let mockJson = { "html_attributions" : [], "next_page_token" : "CrQCIQEAALxUYwrpos9JI6HsyynXEucQkWqTE9KWL7JZB4CTx9iAUcko5iwHFnxByj1o8ZDGodxOPyYJtWezsAxw0ziOl6N_RoWwpkh2TVx4RgDtK8E4_4INyc8-As6trh_0jkInlwr1l1OjQElw7LryzCtE03z_FOi-zbYVaCVu2O0dAvQ6P12csfJtFGS_FKtYyTT_zrfQJDQgNB3QWyVQk2yqlp7PUrSYFWnDNuF-JWKRfd0-I1JIobbp7IjKRQgKLuvThdAUpr2PkL2UbVCp7nrhDXvYjSpVgnvn8XoVjuBp65Aku7aVaJftbImELHrt22Z24hrpNewDtfB_R5mAiXCKmAR7ZILoQMQcrixnesmXEu_szqGRYVIhxWSehHHHpCC1jJmD3F-xxDBU14iwUnCgYN4SECRg7jSjWh23gc8yGsPjt38aFEZPHXGqlpfoL67Wi6TYCARWTJEl", "results" : [ { "geometry" : { "location" : { "lat" : 44.44195029999999, "lng" : 26.0994447 }, "viewport" : { "northeast" : { "lat" : 44.44327878029149, "lng" : 26.1007110302915 }, "southwest" : { "lat" : 44.44058081970849, "lng" : 26.0980130697085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png", "id" : "5b56e0870c9068ecb47a98b3d4d277ff49725896", "name" : "Hotel Ambasador", "photos" : [ { "height" : 2304, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/103698073470443878889/photos\"\u003eH F\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAXebjfVO3d8xZd8wbnBIAaRpL-U5t4-maGX_HbuLxZ5JHi66kM3BzTWVewn8M52A5Drbvh3-oQf2lriLUo_8B_G8rvPjNITqM_A3-6NSvolrslDIMRQuTJ2wGWiHKVzzVEhA1w3cLD9G7ccHKH5vV3bMUGhRtETqBPK-QO65jzD7iJO9blN_4DA", "width" : 3072 } ], "place_id" : "ChIJyxxm507_sUARWzWc4dDySNk", "plus_code" : { "compound_code" : "C3RX+QQ Bucharest, Romania", "global_code" : "8GP8C3RX+QQ" }, "rating" : 4.1, "reference" : "ChIJyxxm507_sUARWzWc4dDySNk", "scope" : "GOOGLE", "types" : [ "lodging", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 699, "vicinity" : "Bulevardul General Gheorghe Magheru 7, București" }, { "geometry" : { "location" : { "lat" : 44.4367992, "lng" : 26.0971437 }, "viewport" : { "northeast" : { "lat" : 44.43814818029149, "lng" : 26.0984926802915 }, "southwest" : { "lat" : 44.43545021970849, "lng" : 26.0957947197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png", "id" : "66a052de34164431971c638f2dad901aa4fc0969", "name" : "Hotel Novotel Bucharest City Centre", "opening_hours" : { "open_now" : true }, "photos" : [ { "height" : 574, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/111872140209169454094/photos\"\u003eHotel Novotel Bucharest City Centre\u003c/a\u003e" ], "photo_reference" : "CmRZAAAAaT-l0JNNui3u2G07VhVAtmRQd8rQ1X2nYdtsLaxlcXKqpquGfrpSWkOql5gagxGtckJyhbk5YhJdIwoaxdTOMo-e7oxSyXVJgGbJXplM_1jYbszVg4mUGjbFv-dDPv_9EhBosNC3PRB6OfhH0N1E7JtAGhR6Pb8QIQAPYUAR_vl-uP3LLDy7cA", "width" : 574 } ], "place_id" : "ChIJ4ewzl0b_sUAR0pV8a8Qn-xw", "plus_code" : { "compound_code" : "C3PW+PV Bucharest, Romania", "global_code" : "8GP8C3PW+PV" }, "rating" : 4.5, "reference" : "ChIJ4ewzl0b_sUAR0pV8a8Qn-xw", "scope" : "GOOGLE", "types" : [ "lodging", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 2509, "vicinity" : "Calea Victoriei 37B, București" }, { "geometry" : { "location" : { "lat" : 44.42741609999999, "lng" : 26.1117542 }, "viewport" : { "northeast" : { "lat" : 44.42867743029149, "lng" : 26.1130849802915 }, "southwest" : { "lat" : 44.42597946970849, "lng" : 26.1103870197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png", "id" : "9f4e3c58ab34f0c358c9547f8faaa40727f64666", "name" : "Mercure Bucharest Unirii", "opening_hours" : { "open_now" : false }, "photos" : [ { "height" : 853, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/109436618545339667544/photos\"\u003eMercure Bucharest Unirii\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAjjPPTy_eoB6tKSkt1aM6h-axB0dm4zOi71WxAshwBhlnn3VqUyv-iLpMdZLQe03D6aZyEnGrOMM30M8QvwoZCqmw4pHFExC8u2ulJ-pyNj_0U6T63mhq01oRIawvbjTFEhAY57-5HRv6j7cmSoHqCbIuGhQwvoRpvNcAhTBdiwyQ_xRkG8n9sg", "width" : 1280 } ], "place_id" : "ChIJUZ9ChxX_sUAR_mwheNeSFlc", "plus_code" : { "compound_code" : "C4G6+XP Bucharest, Romania", "global_code" : "8GP8C4G6+XP" }, "rating" : 4.1, "reference" : "ChIJUZ9ChxX_sUAR_mwheNeSFlc", "scope" : "GOOGLE", "types" : [ "lodging", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 683, "vicinity" : "28th, Bulevardul Mircea Vodă, București" }, { "geometry" : { "location" : { "lat" : 44.4413868, "lng" : 26.0944712 }, "viewport" : { "northeast" : { "lat" : 44.4427698302915, "lng" : 26.0958818802915 }, "southwest" : { "lat" : 44.4400718697085, "lng" : 26.0931839197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png", "id" : "536cb3008bf7dd8c1feab4ad77b7420559fc1e53", "name" : "Radisson Blu Hotel, Bucharest", "opening_hours" : { "open_now" : true }, "photos" : [ { "height" : 289, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/110405397872326312976/photos\"\u003eRadisson Blu Hotel, Bucharest\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAFIlM9HpyKBLEfou9biJMxsfHtnPTCG3hQm7YcqmJBtgHLznOXQCui0W6wuvBrCeQSGBC5ED8oRtKEx8_OrfDOle2aM-OEnpQ49FYGMGBlz-MzccxWk90OXrRdllKucj-EhDmR8My6cb2uI1fbaR2QLbVGhTwOT7D5CzpYBunF7Jz5KnfpZIxvw", "width" : 512 } ], "place_id" : "ChIJEXYoWEX_sUARwQnfVA7TqqQ", "plus_code" : { "compound_code" : "C3RV+HQ Bucharest, Romania", "global_code" : "8GP8C3RV+HQ" }, "rating" : 4.5, "reference" : "ChIJEXYoWEX_sUARwQnfVA7TqqQ", "scope" : "GOOGLE", "types" : [ "spa", "bar", "lodging", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 4480, "vicinity" : "Calea Victoriei 63-81, București" }, { "geometry" : { "location" : { "lat" : 44.43514189999999, "lng" : 26.1180214 }, "viewport" : { "northeast" : { "lat" : 44.43646348029149, "lng" : 26.1194078302915 }, "southwest" : { "lat" : 44.4337655197085, "lng" : 26.11670986970849 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png", "id" : "0be26477a5ad7de1116ccfb3a84422c7e4b01075", "name" : "HOTEL ZAVA", "opening_hours" : { "open_now" : true }, "photos" : [ { "height" : 4032, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/107934205434060403466/photos\"\u003eandreea modoranu\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAKUPEtAdSxsMHOQgJAoSnMjh5LPfMDkiDZHzB5F0gw5Jp24fk2lhQ9iB9vFcFS2a3zZ1RSC0z07ZQZ0ETyloXx5bvyJ2fYMbJF1Cbq1kHNRQZlTEBd7fFinSHbTBYiat2EhDVx7ejn1L4cDF4frQkWbvKGhTGTivgfxEFocYgdhXKJ0oD3npbHA", "width" : 3024 } ], "place_id" : "ChIJ8VFAqzr_sUAR3CA7pPFP3Ys", "plus_code" : { "compound_code" : "C4P9+36 Bucharest, Romania", "global_code" : "8GP8C4P9+36" }, "price_level" : 2, "rating" : 3.9, "reference" : "ChIJ8VFAqzr_sUAR3CA7pPFP3Ys", "scope" : "GOOGLE", "types" : [ "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 121, "vicinity" : "Strada Ștefan Mihăileanu 21, București" }, { "geometry" : { "location" : { "lat" : 44.432372, "lng" : 26.100023 }, "viewport" : { "northeast" : { "lat" : 44.4337335802915, "lng" : 26.1013285302915 }, "southwest" : { "lat" : 44.4310356197085, "lng" : 26.0986305697085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png", "id" : "bd36887b1038cd974380679231b70195c8eb58a9", "name" : "Grand Café Van Gogh", "opening_hours" : { "open_now" : false }, "photos" : [ { "height" : 1150, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/100511209823837847314/photos\"\u003eGrand Café Van Gogh\u003c/a\u003e" ], "photo_reference" : "CmRaAAAACBd6382ixZobzo3g1ji_Cxcs33aiXvUn-5QNPPnVEH1Vu7tU-nIY3i1tGquRjv-cf4uDwdQKh4n_e95vpfkuyRAf_NfTUVepMckDmf34dSuM-ed98kNAJ0Kl7Tav97xZEhAXfGRQytRiPDsevhZNXqhKGhSTpVMV4U6g1R1gUbjjkrgWtq2F2Q", "width" : 1736 } ], "place_id" : "ChIJM5g_jj__sUARVh11WU_h-nQ", "plus_code" : { "compound_code" : "C4J2+W2 Bucharest, Romania", "global_code" : "8GP8C4J2+W2" }, "price_level" : 2, "rating" : 4.3, "reference" : "ChIJM5g_jj__sUARVh11WU_h-nQ", "scope" : "GOOGLE", "types" : [ "cafe", "bar", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 1954, "vicinity" : "Strada Smârdan 9, București" }, { "geometry" : { "location" : { "lat" : 44.4319055, "lng" : 26.1025903 }, "viewport" : { "northeast" : { "lat" : 44.4332552802915, "lng" : 26.1039181802915 }, "southwest" : { "lat" : 44.43055731970851, "lng" : 26.1012202197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png", "id" : "ae4751c01584279862d97ccb29adc271560cc71a", "name" : "La Mama", "opening_hours" : { "open_now" : false }, "photos" : [ { "height" : 3456, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/114320951683290626703/photos\"\u003eImmacolata Abategiovanni\u003c/a\u003e" ], "photo_reference" : "CmRaAAAA3pnHHEAkEiQqAo4Y41F5kncBbTM9hK3ZCTDiQ67XNxfH-t44_RXA8dM-tJ7-NsOuCiQDrNL84lUhCehemLJlbc54jh0x8jyzs0VXETtAm8QFsHoTQKNmrPhaIyJp94x9EhC7sTGBiiuju9eZ6Uo3MyeyGhSsRsJRlCJYPaMbp5SUWpqLeM7d7A", "width" : 4608 } ], "place_id" : "ChIJ_ZJhBz__sUAR5_lJTfKBafs", "plus_code" : { "compound_code" : "C4J3+Q2 Bucharest, Romania", "global_code" : "8GP8C4J3+Q2" }, "price_level" : 2, "rating" : 3.9, "reference" : "ChIJ_ZJhBz__sUAR5_lJTfKBafs", "scope" : "GOOGLE", "types" : [ "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 1093, "vicinity" : "Strada Băcani 1, București" }, { "geometry" : { "location" : { "lat" : 44.4305621, "lng" : 26.10091929999999 }, "viewport" : { "northeast" : { "lat" : 44.4319277802915, "lng" : 26.1021438802915 }, "southwest" : { "lat" : 44.4292298197085, "lng" : 26.0994459197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png", "id" : "6c68583a7c79013088d8b33401c60d5a87b70598", "name" : "Curtea Berarilor", "opening_hours" : { "open_now" : true }, "photos" : [ { "height" : 1440, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/104915703581044211298/photos\"\u003ealexandra nicolae\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAPeBgmv4mYjPSzQPJIGmn38B20v96ZY77uQLKLUNV3Qfqrjk-IXOrmIoi6011MhzSw0J3a2wOfaGtZWDxLrf2p7jkbKrX8MfYdBJAAL2BMHhyXv7kohAxoPO4KF5RsgssEhBDV8F-sjI1bU_qZEiHwglbGhRUO7Pvo4xiA6SAS8PRjbAYqGJ4NA", "width" : 2560 } ], "place_id" : "ChIJt0sqxD__sUAR7gcIoiuNcvU", "plus_code" : { "compound_code" : "C4J2+69 Bucharest, Romania", "global_code" : "8GP8C4J2+69" }, "price_level" : 2, "rating" : 4.2, "reference" : "ChIJt0sqxD__sUAR7gcIoiuNcvU", "scope" : "GOOGLE", "types" : [ "bar", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 2091, "vicinity" : "Strada Șelari 9-11, București" }, { "geometry" : { "location" : { "lat" : 44.4307615, "lng" : 26.10183039999999 }, "viewport" : { "northeast" : { "lat" : 44.4320963302915, "lng" : 26.1031660802915 }, "southwest" : { "lat" : 44.4293983697085, "lng" : 26.1004681197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png", "id" : "b140179a2d415ca0d1246fee398708b0b15a3e76", "name" : "Fire Club", "opening_hours" : { "open_now" : true }, "photos" : [ { "height" : 3000, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/115128650778906210055/photos\"\u003eJesus A.\u003c/a\u003e" ], "photo_reference" : "CmRaAAAA2kxI44c7_CvVYl5GfHurh4EEsvuCbsm7fzdtsVbFkc5LBJBgKjbSH2c5qkbz5i9-UMxubH0U2qTcyVVBeOppZvD2wRzXvdDZraUGHtKxO4srRy9VQinwanaLhsQNLj5MEhD7YBi2wctTCDinuuIiwym5GhSKYtA0-3BaWofdgFe9eIM4oo9KRQ", "width" : 4000 } ], "place_id" : "ChIJ9cMirj__sUARQDw7n0EPmos", "plus_code" : { "compound_code" : "C4J2+8P Bucharest, Romania", "global_code" : "8GP8C4J2+8P" }, "rating" : 4.4, "reference" : "ChIJ9cMirj__sUARQDw7n0EPmos", "scope" : "GOOGLE", "types" : [ "bar", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 2268, "vicinity" : "Strada Covaci 7, București" }, { "geometry" : { "location" : { "lat" : 44.4365047, "lng" : 26.0988989 }, "viewport" : { "northeast" : { "lat" : 44.4378536802915, "lng" : 26.1002478802915 }, "southwest" : { "lat" : 44.4351557197085, "lng" : 26.0975499197085 } } }, "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png", "id" : "f07a8309441629e421f7fe60e11ac7d21b4ebaf4", "name" : "Pizza Colosseum", "opening_hours" : { "open_now" : false }, "photos" : [ { "height" : 1280, "html_attributions" : [ "\u003ca href=\"https://maps.google.com/maps/contrib/104572317671370082004/photos\"\u003eJulie\u003c/a\u003e" ], "photo_reference" : "CmRaAAAAWkhwuAk1PLB1sWiBRE33_0-3rg9ckzZDVISjnGfd0dlJ-ZLAlMY_MqpARaVaCrv24SKMuxkKEmYw5uQwGDuHIuGov8-OnTV7sSv1NboIzk9CWfWB4Y5tNt8kWV1YX6XKEhAUvadcZVw2TZ3IfUnoLuONGhTU3DEDAysdKR5DynfsuG1wJVJUMQ", "width" : 1920 } ], "place_id" : "ChIJPcdm6Eb_sUARN2KZ1WiXrzY", "plus_code" : { "compound_code" : "C3PX+JH Bucharest, Romania", "global_code" : "8GP8C3PX+JH" }, "rating" : 4, "reference" : "ChIJPcdm6Eb_sUARN2KZ1WiXrzY", "scope" : "GOOGLE", "types" : [ "meal_delivery", "restaurant", "point_of_interest", "food", "establishment" ], "user_ratings_total" : 351, "vicinity" : "Calea Victoriei, Pasaj Victoria nr. 48-50, București" } ], "status" : "OK" };
+    
 
 // test route 
 app.get('/places',  (req, res) => {
@@ -144,8 +29,7 @@ app.get('/places',  (req, res) => {
         if(error) {
             res.send('An error occured')
         } else {
-            console.log(body);
-            res.send(mockJson)
+            res.send(mockJson);
         }
     });
 });
